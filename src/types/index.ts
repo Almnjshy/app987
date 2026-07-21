123589de6982cbd060d568eff5800ecec6e2cecb
Almnjshy <115098081+Almnjshy@users.noreply.github.com>
2026-07-21 23:41:57 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,2 @@
-export * from './tile';
-export * from './board';
-export * from './gameengine';
-export * from './layout';
-export * from './ai';
-export * from './tests';
+// src/types/index.ts
+export * from './game';
